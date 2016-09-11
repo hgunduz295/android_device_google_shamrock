b90b26390a0c7dde853746b8cc7b2a5a486da68e
Deftones <punisher_caner@windowslive.com>
2016-09-11 18:33:29 +0000

    Optionally Build TWRP

--- board/recovery.mk
+++ board/recovery.mk
@@ -1,5 +1,7 @@
 # Recovery
 TARGET_RECOVERY_FSTAB := device/google/shamrock/rootdir/etc/fstab.qcom
-RECOVERY_VARIANT := twrp
-TW_THEME := portrait_hdpi
-PRODUCT_COPY_FILES += device/google/shamrock/twrp.fstab:recovery/root/etc/twrp.fstab
+
+#Enable Below Lines For Build TWRP
+#RECOVERY_VARIANT := twrp
+#TW_THEME := portrait_hdpi
+#PRODUCT_COPY_FILES += device/google/shamrock/twrp.fstab:recovery/root/etc/twrp.fstab
