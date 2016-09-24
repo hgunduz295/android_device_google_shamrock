ffe78ef9867d69275d0b2b41a82b1e96c65bb82b
Wzedlare <vedatak01@gmail.com>
2016-09-24 19:47:52 +0300

    Some Fixes for cm14 #2

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,3 +19,7 @@ include device/google/shamrock/board/*.mk
 
 # inherit from the proprietary version
 -include vendor/google/shamrock/BoardConfigVendor.mk
+
+#Yarramı Ye Edition Edit By Raycebin
+
+
