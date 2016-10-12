d8baf1d1a812cd6f7d62a28f54add7f68b9e9dbb
Deftones <punisher_caner@windowslive.com>
2016-10-12 10:36:28 +0000

    Bluetooth Fix @Kayan

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -19,5 +19,18 @@
 
 #define BTM_DEF_LOCAL_NAME   "GM 5 Plus"
 #define BLUETOOTH_QTI_SW TRUE
-#define BLE_VND_INCLUDED   FALSE
+#define BTIF_HF_WBS_PREFERRED TRUE
+#define BTM_WBS_INCLUDED TRUE
+#define BTA_SKIP_BLE_READ_REMOTE_FEAT TRUE
+#define BTA_HOST_INTERLEAVE_SEARCH TRUE
+#define MAX_L2CAP_CHANNELS 16
+
+// skips conn update at conn completion
+#define BTA_BLE_SKIP_CONN_UPD TRUE
+#define BTA_DISABLE_DELAY 100 /* in milliseconds */
+#define BTM_BLE_ADV_TX_POWER {-21, -15, -7, 1, 9}
+#define BLE_PERIPHERAL_ADV_NAME FALSE
+#define BTM_LE_SECURE_CONN TRUE
+#define BT_CLEAN_TURN_ON_DISABLED 1
+
 #endif
