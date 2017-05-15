a0085639f6e2f892d89940772d0bf75b80ed0300
Harry Youd <harry@harryyoud.co.uk>
2017-05-15 08:22:05 +0100

    Remove Gello from PRODUCT_PACKAGES
    Change-Id: I5a1ce8b09ba9d3d3e9e2e49031413cd2810885b9

--- device.mk
+++ device.mk
@@ -124,10 +124,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/etc/init.qcom.bt.sh:system/etc/init.qcom.bt.sh
 
-# Browser
-PRODUCT_PACKAGES += \
-    Gello
-
 # Camera
 PRODUCT_PACKAGES += \
     Camera2
